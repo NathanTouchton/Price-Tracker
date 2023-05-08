4599a807ba3e6138ea6c74db9cc3c89bc81d220e
Nathan Touchton <ntouchton@protonmail.com>
2023-05-08 16:25:12 -0500

    new file:   emailtemplate.py 	modified:   main.py

---
+++ emailtemplate.py
@@ -0,0 +1,32 @@
+"""This is a module for more easily setting up code to send an email with SMTP."""
+
+from os import environ
+from smtplib import SMTP
+
+class SendEmail:
+    """Primary class for the email template module.
+    Please provide the smtp server for your email service.
+    IMPORTANT!!! You will need environment variables set up in the following way:
+    EMAIL_ADDRESS='your email address'
+    EMAIL_PASSWORD='your email password"""
+    def __init__(self, smtp_server):
+        self.email = environ["EMAIL_ADDRESS"]
+        self.password = environ["EMAIL_PASSWORD"]
+        self.smtp = str(smtp_server)
+
+    def send_message(self, to_email, message):
+        """This method sends an email using current environment variables.
+        You need to provide the recipient's email and the content of the message.
+        Per the SMTP class rules, to add a subject, type 'subject:' and then do 2 new lines
+        followed by the contents of the message."""
+        with SMTP(self.smtp, port=587) as connection:
+            connection.starttls()
+            connection.login(
+                self.email,
+                self.password,
+                )
+            connection.sendmail(
+                from_addr=self.email,
+                to_addrs=to_email,
+                msg=message,
+                )
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-# from pprint import pprint
+from smtplib import SMTP
+from os import environ
 from requests import get
 from bs4 import BeautifulSoup
 
-URL = "https://drop.com/buy/massdrop-x-sennheiser-hd-58x-jubilee-headphones"
+EMAIL = environ["user"]
+TO_EMAIL = environ["to_email"]
+PASSWORD = environ["password"]
+URL = "https://pcpartpicker.com/product/gjpQzy/corsair-vengeance-16-gb-2-x-8-gb-ddr5-4800-sodimm-cl40-memory-cmsx16gx5m2a4800c40"
 
 response = get(
     url=URL,
@@ -14,6 +18,23 @@ response = get(
     )
 soup = BeautifulSoup(response.text, "html.parser")
 
-price_text = soup.find(class_="wdio__price Text__text__PazWx Text__type--price__1mumP")
+# This finds the class that PC Part Picker seems to use for the lowest price
+# and converts it to a floating point number.
+price_text = float(soup.find(class_="td__base priority--2").text.split("$")[1])
 
-print(price_text)
+# print(price_text)
+
+# $60 is the target price.
+# This if statement sends an email once the price gets below the target. ($60, in this case.)
+if price_text <= 60:
+    with SMTP("smtp.mail.yahoo.com", port=587) as connection:
+        connection.starttls()
+        connection.login(
+            EMAIL,
+            PASSWORD
+            )
+        connection.sendmail(
+            from_addr=EMAIL,
+            to_addrs=TO_EMAIL,
+            msg=f"Subject: Buy the thing.\n\nBuy the thing. {URL}"
+            )
